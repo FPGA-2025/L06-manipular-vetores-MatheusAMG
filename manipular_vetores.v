cf62b652523dd92bee298752288738e301fd6ddc
Matheus <matheusamg@gmail.com>
2025-04-14 11:45:15 -0300

    passou em tudo

--- manipular_vetores.v
+++ manipular_vetores.v
@@ -2,6 +2,9 @@ module manipular_vetores(
     input [31:0] entrada,
     output [31:0] saida );
 
-    // insira seu código aqui
+assign saida[7:0] = entrada [31:24];
+assign saida[15:8] = entrada [23:16];
+assign saida[23:16] = entrada [15:8];
+assign saida[31:24] = entrada [7:0];
 
 endmodule
